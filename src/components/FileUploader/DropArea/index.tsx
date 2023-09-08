b0ea2afb03a4783f42a78c99acc30d781c5336ce
ohwonjae <hdnjswo5000@douzone.com>
2023-09-08 21:43:29 +0900

    [feature/inputFile 기능 구현] - Drag&Drop 시 파일 세팅 테스트 코드 작성 - Drag&Drop 시 파일 세팅 구현 - Drag&Drop 시 이미지가 아닌 파일 예외처리 테스트 코드 작성 - Drag&Drop 시 이미지가 아닌 파일 예외처리 구현

--- src/components/FileUploader/DropArea/index.tsx
+++ src/components/FileUploader/DropArea/index.tsx
@@ -2,17 +2,43 @@ import './DropArea.css';
 import uploaderImg from '/public/assets/uploader_icon.png';
 import { FilesAtom } from '@/atoms/FileUploader/atoms';
 import { useAtom } from 'jotai';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useRef } from 'react';
 export const DropArea = () => {
   const [files, setFiles] = useAtom(FilesAtom);
 
+  const checkImageFileType = (fileName: string) => {
+    const standards = ['png', 'jpg', 'jpeg', 'png', 'svg'];
+    const extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+    for (const s of standards) {
+      if (s === extension) {
+        return true;
+      }
+    }
+    return false;
+  };
   const handleOnFileList = (e: ChangeEvent<HTMLInputElement>) => {
     const arr = [...e.target.files] as File[];
     setFiles((files) => [...files, ...arr]);
   };
-
   return (
-    <div className={'drop-area'} onMouse>
+    <div
+      className={'drop-area'}
+      onDrop={(e) => {
+        e.preventDefault();
+        for (let i = 0; i < e.dataTransfer.files.length; i++) {
+          if (!checkImageFileType(e.dataTransfer.files[i].name)) {
+            alert('이미지 파일을 넣어주세요');
+            return;
+          }
+        }
+        setFiles((f) => {
+          return [...f, ...e.dataTransfer.files];
+        });
+      }}
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+    >
       <div className={'uploader-img'}>
         <img src={uploaderImg} />
         <div className={'uploader-text'}>Drag & Drop</div>
--- src/components/FileUploader/FileList/FileList.test.tsx
+++ src/components/FileUploader/FileList/FileList.test.tsx
@@ -1,13 +1,7 @@
 import { FileList } from './index';
 import { DropArea } from '../DropArea';
 import React from 'react';
-import {
-  render,
-  getByText,
-  screen,
-  fireEvent,
-  getByLabelText,
-} from '@testing-library/react';
+import { render, getByText, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('FileList Test', () => {
@@ -27,26 +21,6 @@ describe('FileList Test', () => {
     const inputFile = screen.getByLabelText(/파일 탐색기/i, { exact: false });
     const fakeFile = new File(['hello'], 'hello.png', { type: 'image/png' });
     await userEvent.upload(inputFile, [fakeFile]);
-    screen.getByText(/hello.png/i, { exact: true });
-  });
-});
-
-describe('Drag & Drop Test', () => {
-  let _container: HTMLElement | null = null;
-  beforeEach(() => {
-    const { container } = render(<DropArea />);
-    _container = container;
-  });
-  it('Drag&Drop시 파일 세팅됨', () => {
-    if (_container == null) return;
-    const DragDrop = getByText(_container, 'Drag & Drop');
-    expect(DragDrop).toBeInTheDocument();
-    const fakeFile = new File(['hello'], 'hello.png', { type: 'image/png' });
-    fireEvent.drop(DragDrop, {
-      dataTransfer: {
-        files: [fakeFile],
-      },
-    });
     screen.getByText(/hello.png/i, { exact: true });
   });
 });
